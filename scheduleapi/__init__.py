e65099422fa2da7d9ed978377293aba8cd0aa28c
Cruor99 <kliknes@gmail.com>
2017-10-28 00:23:54 +0200

    initial commit

---
+++ scheduleapi/__init__.py
@@ -0,0 +1,59 @@
+#! ../env/bin/python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Kjetil Andre Liknes'
+__email__ = 'kliknes@gmail.com'
+__version__ = '0.1'
+
+from flask import Flask
+from webassets.loaders import PythonLoader as PythonAssetsLoader
+
+from scheduleapi.controllers.main import main
+from scheduleapi import assets
+from scheduleapi.models import db
+
+from scheduleapi.extensions import (
+    cache,
+    assets_env,
+    debug_toolbar,
+    login_manager
+)
+
+
+def create_app(object_name):
+    """
+    An flask application factory, as explained here:
+    http://flask.pocoo.org/docs/patterns/appfactories/
+
+    Arguments:
+        object_name: the python path of the config object,
+                     e.g. scheduleapi.settings.ProdConfig
+
+        env: The name of the current environment, e.g. prod or dev
+    """
+
+    app = Flask(__name__)
+
+    app.config.from_object(object_name)
+
+    # initialize the cache
+    cache.init_app(app)
+
+    # initialize the debug tool bar
+    debug_toolbar.init_app(app)
+
+    # initialize SQLAlchemy
+    db.init_app(app)
+
+    login_manager.init_app(app)
+
+    # Import and register the different asset bundles
+    assets_env.init_app(app)
+    assets_loader = PythonAssetsLoader(assets)
+    for name, bundle in assets_loader.load_bundles().items():
+        assets_env.register(name, bundle)
+
+    # register our blueprints
+    app.register_blueprint(main)
+
+    return app
